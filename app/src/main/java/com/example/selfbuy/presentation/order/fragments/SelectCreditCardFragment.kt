5c97e34ed3b4ad89aa9d16cc67e55ec40fc09ec7
nicolas laroche <nicolas.laroche@ynov.com>
2020-04-15 16:32:18 +0200

    check status intent - En cours

--- app/src/main/java/com/example/selfbuy/presentation/order/fragments/SelectCreditCardFragment.kt
+++ app/src/main/java/com/example/selfbuy/presentation/order/fragments/SelectCreditCardFragment.kt
@@ -1,12 +1,12 @@
 package com.example.selfbuy.presentation.order.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.example.selfbuy.R
 import com.example.selfbuy.data.entity.remote.*
@@ -17,20 +17,23 @@ import com.example.selfbuy.room.Async
 import com.example.selfbuy.room.entity.Product
 import com.example.selfbuy.utils.ManageThread
 import com.google.android.material.snackbar.Snackbar
+import com.stripe.android.PaymentConfiguration
+import com.stripe.android.Stripe
+import com.stripe.android.model.StripeIntent
 import kotlinx.android.synthetic.main.fragment_select_credit_card.*
 
 class SelectCreditCardFragment : Fragment() {
 
-    private val creditCards : MutableList<String> = arrayListOf()
-    private var selectedCreditCard : CreditCardDto? = null
-    private var listCreditCardUser : ArrayList<CreditCardDto> = arrayListOf()
+    private val creditCards: MutableList<String> = arrayListOf()
+    private var selectedCreditCard: CreditCardDto? = null
+    private var listCreditCardUser: ArrayList<CreditCardDto> = arrayListOf()
     private val creditCardViewModel = CreditCardViewModel()
-    private var isFirstLoad : Boolean = true
+    private var isFirstLoad: Boolean = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? =inflater.inflate(R.layout.fragment_select_credit_card, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_select_credit_card, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -47,11 +50,9 @@ class SelectCreditCardFragment : Fragment() {
     /**
      * Evenement du click sur le bouton de confirmation de commande
      */
-    private fun btnConfirmClickListener()
-    {
+    private fun btnConfirmClickListener() {
         btn_confirm_command.setOnClickListener {
-            if (selectedCreditCard != null)
-            {
+            if (selectedCreditCard != null) {
                 progressBar_list_cards_user.visibility = View.VISIBLE
                 Async {
                     val resultQuery = SFApplication.app.dbRoom.productDao().getAll()
@@ -68,19 +69,13 @@ class SelectCreditCardFragment : Fragment() {
                     }
                 }.execute()
             }
-//            val orderConfirmed = OrderConfirmedFragment()
-//            val fragmentTransaction = activity!!.supportFragmentManager.beginTransaction().apply {
-//                replace(R.id.select_credit_card_activity_fragment_container, orderConfirmed)
-//                addToBackStack(null)
-//            }
-//            fragmentTransaction.commit()
         }
     }
 
     /**
      * Abonnement aux evenements de selection du spinner des cartes de credits
      */
-    private fun spinnerItemOnClickListener(){
+    private fun spinnerItemOnClickListener() {
         spinner_credit_cards.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parentView: AdapterView<*>?,
@@ -88,14 +83,10 @@ class SelectCreditCardFragment : Fragment() {
                 position: Int,
                 id: Long
             ) {
-                if (isFirstLoad && listCreditCardUser.size>0)
-                {
+                if (isFirstLoad && listCreditCardUser.size > 0) {
                     selectedCreditCard = listCreditCardUser.find { it.isDefaultCard }
-                }
-                else
-                {
+                } else {
                     selectedCreditCard = listCreditCardUser[position]
-                    view?.let { v -> Snackbar.make(v, selectedCreditCard!!._id, Snackbar.LENGTH_SHORT).show() }
                 }
             }
 
@@ -108,17 +99,18 @@ class SelectCreditCardFragment : Fragment() {
     /**
      * Recupere le prix total du panier de l'utilisateur
      */
-    private fun getTotalPrices(){
+    private fun getTotalPrices() {
         Async {
             val resultQuery = SFApplication.app.dbRoom.productDao().getAll()
 
-            ManageThread.executeOnMainThread(this.context!!){
+            ManageThread.executeOnMainThread(this.context!!) {
                 var totalPrice = 0.0
                 resultQuery.forEach { product: Product ->
                     totalPrice += product.price * product.quantity
                 }
 
-                val totalPriceToDisplay = "${"%.2f".format(totalPrice)}${getString(R.string.euro_symbol)}"
+                val totalPriceToDisplay =
+                    "${"%.2f".format(totalPrice)}${getString(R.string.euro_symbol)}"
                 tw_total_select_credit_card.text = totalPriceToDisplay
             }
         }.execute()
@@ -133,13 +125,16 @@ class SelectCreditCardFragment : Fragment() {
             Observer { resultDto: ResultApiDto<ArrayList<CreditCardDto>> ->
                 progressBar_list_cards_user.visibility = View.GONE
 
-                if (resultDto.data != null)
-                {
+                if (resultDto.data != null) {
                     listCreditCardUser = resultDto.data
-                    resultDto.data.forEach{
+                    resultDto.data.forEach {
                         creditCards.add("**** **** **** ${it.last4}           ${it.expMonth}/${it.expYear}")
                     }
-                    val dataAdapter: ArrayAdapter<String> = ArrayAdapter(context!!, R.layout.support_simple_spinner_dropdown_item, creditCards)
+                    val dataAdapter: ArrayAdapter<String> = ArrayAdapter(
+                        context!!,
+                        R.layout.support_simple_spinner_dropdown_item,
+                        creditCards
+                    )
                     dataAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
                     spinner_credit_cards.adapter = dataAdapter
                 }
@@ -154,13 +149,48 @@ class SelectCreditCardFragment : Fragment() {
         creditCardViewModel.paymentIntentLiveData.observe(
             viewLifecycleOwner,
             Observer { resultDto: ResultApiDto<PaymentIntentDto> ->
-                progressBar_list_cards_user.visibility = View.GONE
+                Thread {
+                    //Do some Network Request
+                    ManageThread.executeOnMainThread(this.context!!)
+                    {
+                        progressBar_list_cards_user.visibility = View.GONE
 
-                if (resultDto.data != null)
-                {
-                    view?.let { v -> Snackbar.make(v, "${resultDto.data.clientSecret} -- ${resultDto.data.paymentIntentId}", Snackbar.LENGTH_SHORT).show() }
+                        if (resultDto.data != null) {
+                            view?.let { v ->
+                                Snackbar.make(
+                                    v,
+                                    "${resultDto.data.clientSecret} -- ${resultDto.data.paymentIntentId}",
+                                    Snackbar.LENGTH_SHORT
+                                ).show()
+                            }
 
-                }
-            })
+                            val stripe = Stripe(
+                                this.context!!,
+                                PaymentConfiguration.getInstance(this.context!!).publishableKey
+                            )
+                            val result =
+                                stripe.retrievePaymentIntentSynchronous(resultDto.data.clientSecret)
+
+                            //                    ManageThread.executeOnMainThread(this.context!!){
+                            //                        if (result != null && result.status == StripeIntent.Status.Succeeded && result.nextAction == null)
+                            //                        {
+                            //                            val orderConfirmed = OrderConfirmedFragment()
+                            //                            val fragmentTransaction = activity!!.supportFragmentManager.beginTransaction().apply {
+                            //                                replace(R.id.select_credit_card_activity_fragment_container, orderConfirmed)
+                            //                                addToBackStack(null)
+                            //                            }
+                            //                            fragmentTransaction.commit()
+                            //                        }
+                            //                        else
+                            //                        {
+                            //                            //WebView 3D secure
+                            //                        }
+                            //                    }
+                        }
+                    }}.start()
+                })
+
     }
+
+
 }
