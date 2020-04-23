18d736589a2e3cd8c0388e32aa10ecb93ffdb7bd
Gael Bosser <bosser.gael@gmail.com>
2020-04-23 21:00:02 +0200

    Disable mode nuit

--- app/src/main/java/com/example/selfbuy/presentation/splashScreen/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/example/selfbuy/presentation/splashScreen/activity/SplashScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.selfbuy.R
 import com.example.selfbuy.presentation.home.activity.HomeActivity
 
@@ -12,6 +13,8 @@ class SplashScreenActivity : AppCompatActivity() {
     private val SPLASH_TIME_OUT:Long=1000 // 1 sec
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
